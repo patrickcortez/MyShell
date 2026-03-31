813b59a06d456e15e8fb0642fa15f71f46db1890
patrickcortez <patrickcortez2024@gmail.com>
2026-03-31 18:09:06 +0800

    added io layer and updated structure and status md files

---
+++ commit.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+:: Git helper script 
+
+git --version >nul 2>&1
+
+if %ERRORLEVEL% EQU 0 (
+
+    git add -A
+
+    if "%~1" == "" (
+        echo "Commit Message cannot be empty"
+    ) else (
+        git commit -m "%~1"
+    )
+) else (
+    echo "Git not installed!"
+)
